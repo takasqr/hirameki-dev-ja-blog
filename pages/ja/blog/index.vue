49992fe4db0fb3af132f25a05142b32f713257d9
takasqr <95556025+takasqr@users.noreply.github.com>
2024-10-15 06:03:18 +0900

    chore: ブログ記事一覧ページ作成

---
+++ pages/ja/blog/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="bg-white">
+    <NuxtLayout>
+
+      <main>
+        <ArticlesList/>
+      </main>
+    </NuxtLayout>
+  </div>
+</template>
+
+<script setup>
+import ArticlesList from '../../../components/content/ArticlesList.vue';
+
+definePageMeta({
+  layout: 'ja-article'
+})
+</script>
