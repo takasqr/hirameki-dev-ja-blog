efaa564e1c210750a08d22fbd08ee44cee97eec3
takasqr <95556025+takasqr@users.noreply.github.com>
2024-10-22 12:33:46 +0900

    chore: pages ディレクトリでは必ずヘッダー情報を設定しなければならない

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,20 @@
 import withNuxt from './.playground/.nuxt/eslint.config.mjs'
+import patternRules from 'eslint-plugin-pattern-rules'
 
-export default withNuxt({
-  rules: {
-    '@typescript-eslint/ban-ts-comment': 'off',
+export default withNuxt(
+  {
+    rules: {
+      '@typescript-eslint/ban-ts-comment': 'off',
+    },
   },
-})
+  // pages ディレクトリでは必ずヘッダー情報を設定しなければならない
+  {
+    plugins: {
+      'pattern-rules': patternRules
+    },
+    files: ["pages/**/*.vue"],
+    rules: {
+      'pattern-rules/required-identifier': ['warn', { patterns: ['useSetHead'] }]
+    },
+  },
+)
