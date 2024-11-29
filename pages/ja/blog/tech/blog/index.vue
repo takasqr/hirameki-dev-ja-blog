4e2e7663403a00b5e50994f179b2e5eb4a036ea3
takasqr <95556025+takasqr@users.noreply.github.com>
2024-11-29 11:50:54 +0900

    Create index.vue

---
+++ pages/ja/blog/tech/blog/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="bg-white">
+    <NuxtLayout>
+      <main>
+        <CategoryPageView :category="category" />
+      </main>
+    </NuxtLayout>
+  </div>
+</template>
+
+<script setup lang="ts">
+import CategoryPageView from '../../../../../components/content/category-page-view/CategoryPageView.vue'
+
+const category = 'blog'
+
+definePageMeta({
+  layout: 'ja-article'
+})
+
+useSetHead(getTechBlogCategoryHeadParams(category))
+</script>
