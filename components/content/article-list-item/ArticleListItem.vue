2943339124a1eb44aef45e038af86f7b913c0dd7
takasqr <95556025+takasqr@users.noreply.github.com>
2025-02-13 04:25:19 +0900

    feat: 記事カードの画像を遅延読み込み

--- components/content/article-list-item/ArticleListItem.vue
+++ components/content/article-list-item/ArticleListItem.vue
@@ -5,6 +5,7 @@
         <img
           :src="props.article.cover"
           alt=""
+          loading="lazy"
           class="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
         >
         <div class="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
