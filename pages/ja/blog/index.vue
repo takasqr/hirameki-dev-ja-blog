b4273502f58f3bdca5852a0909e90005a84a5d02
takasqr <95556025+takasqr@users.noreply.github.com>
2025-02-22 06:44:18 +0900

    feat: 記事一覧にページネーションを追加

--- pages/ja/blog/index.vue
+++ pages/ja/blog/index.vue
@@ -3,6 +3,9 @@
     <NuxtLayout>
       <main>
         <ArticleList :articles="articlesData" />
+        <div class="mt-10">
+          <Pagination :next-path="nextPagePath" :previous-path="previousPagePath" />
+        </div>
       </main>
     </NuxtLayout>
   </div>
@@ -12,18 +15,33 @@
 import { withTrailingSlash } from 'ufo'
 import ArticleList from '../../../components/content/article-list/ArticleList.vue';
 import type { Article } from '../../../components/types/Article';
+import Pagination from '@takasqr/tw-vue-ui/basic/pagination/Pagination.vue'
+import useSetHead from '../../../composables/useSetHead'
 
 definePageMeta({
   layout: 'ja-article'
 })
 
+const route = useRoute()
+const page = route.query.page ? String(route.query.page)[0] : null
+
+const perPage = 12
+
 // 非同期データ取得のための一意のキーを作成
 const asyncDataKey = `articles-blog-all`
 const path = '/ja/blog/'
 
+const nextPageNumber = getNextPage(page)
+const nextPagePath = getPagePath(nextPageNumber)
+
+const previousPageNumber = getPreviousPage(page)
+const previousPagePath = getPagePath(previousPageNumber)
+
 const { data: articles } = await useAsyncData(asyncDataKey, async () => {
   const rawContent = await queryContent(withTrailingSlash(path))
     .where({ _partial: false })
+    .skip((Number(page) - 1) * perPage)
+    .limit(perPage)
     .sort({ createDate: -1 })
     .find()
 
@@ -40,4 +58,27 @@ const { data: articles } = await useAsyncData(asyncDataKey, async () => {
 })
 
 const articlesData = articles.value ?? []
+
+function getNextPage(page: string): string | null {
+  const pageNumber = Number(page) + 1
+
+  return String(pageNumber)
+}
+
+function getPreviousPage(page: string): string | null {
+  if (Number(page) <= Number('1')) {
+    return null
+  } else {
+    const pageNumber = Number(page) - 1
+    return String(pageNumber)
+  }
+}
+
+function getPagePath(pageNumber: string): string | null {
+  if (pageNumber == null) {
+    return null
+  } else {
+    return `/ja/blog?page=${pageNumber}`
+  }
+}
 </script>
