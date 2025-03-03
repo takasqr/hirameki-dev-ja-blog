7fd9627e4facb4a89b62286c69011517406a0ffb
takasqr <95556025+takasqr@users.noreply.github.com>
2025-03-04 04:38:21 +0900

    fix: tailwind.config.js の require を import に変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import typography from '@tailwindcss/typography'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -300,6 +302,6 @@ export default {
     },
   },
   plugins: [
-    require('@tailwindcss/typography'),
+    typography,
   ],
 }
