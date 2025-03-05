1b71e0d194fbf825f84a7bf32164c960dc3e910d
takasqr <95556025+takasqr@users.noreply.github.com>
2025-03-05 17:46:52 +0900

    feat: HeaderSignageBlog コンポーネント追加

--- components/custom/general/header/ja-header-view.vue
+++ components/custom/general/header/ja-header-view.vue
@@ -47,23 +47,7 @@
           </div>
 
           <div class="px-4 md:px-0 flex items-center">
-            <span>
-              <a class="underline font-medium" href="/ja/blog/journal/2025/20250210_learn_9_years">9年間毎日プログラミングした話</a>
-
-              <br class="md:hidden">
-
-              <span class="ml-0 md:ml-4 mr-1">by</span>
-              <a href="/ja">
-                <img
-                class="h-4 inline m-0"
-                src="/logo.png"
-                alt=""
-                >
-              </a>
-
-              <a class="underline font-medium ml-2" href="/ja/blog/journal">日常</a>
-              <a class="underline font-medium ml-2" href="/ja/blog/tech">開発</a>
-            </span>
+            <HeaderSignageBlog />
           </div>
 
           <!-- <div class="px-4 md:px-0 prose">
@@ -125,6 +109,7 @@
 import { ref } from 'vue'
 import { Dialog, DialogPanel } from '@headlessui/vue'
 import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/outline'
+import { HeaderSignageBlog } from '../../header-signage/HeaderSignageBlog.vue'
 
 const lang = 'ja'
 
---
+++ components/custom/header-signage/HeaderSignageBlog.vue
@@ -0,0 +1,19 @@
+<template>
+  <span>
+  <a class="underline font-medium" href="/ja/blog/journal/2025/20250210_learn_9_years">9年間毎日プログラミングした話</a>
+
+  <br class="md:hidden">
+
+  <span class="ml-0 md:ml-4 mr-1">by</span>
+    <a href="/ja">
+      <img
+      class="h-4 inline m-0"
+      src="/logo.png"
+      alt=""
+      >
+    </a>
+
+    <a class="underline font-medium ml-2" href="/ja/blog/journal">日常</a>
+    <a class="underline font-medium ml-2" href="/ja/blog/tech">開発</a>
+  </span>
+</template>
