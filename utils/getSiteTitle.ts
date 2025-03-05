767b44b7b844521097dedb2c18c5c2699adc5cd8
takasqr <95556025+takasqr@users.noreply.github.com>
2025-03-05 17:47:18 +0900

    feat: タイトル取得関数変更

--- utils/getSiteTitle.ts
+++ utils/getSiteTitle.ts
@@ -2,6 +2,9 @@ export function getSiteTitle(path: string): string {
   if (path.includes('/ja/blog')) {
     return 'かいがらブログ'
   }
+  else if (path.includes('/en/')) {
+    return 'hirameki.dev'
+  }
   else {
     return 'ひらめき開発'
   }
