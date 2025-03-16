e86790e8911a7df1e9d1d5369f7897a3fe0b546f
takasqr <95556025+takasqr@users.noreply.github.com>
2025-03-17 05:42:15 +0900

    feat: 自作ライブラリの紹介カルーセル

---
+++ components/custom/my-library-carousel/MyLibraryCarousel.vue
@@ -0,0 +1,39 @@
+<template>
+  <Carousel class="rounded-xl">
+    <a href="https://japanjs.org">
+      <img
+        src="https://image.hirameki.dev/takasqr/image/upload/l_dylagecz4vyaeo3xqcdx/l_text:MPLUS1p-Black.ttf_100_bold:JapanJS%0A日本関連%0A便利ツール,co_rgb:374151,w_620,c_fit/v1712091289/ttpfb67cwdp2pwjes6pw.png"
+        alt="JapanJS"
+        loading="lazy"
+      >
+    </a>
+
+    <a href="https://pattern-rules.hirameki.dev/ja">
+      <img
+        src="https://image.hirameki.dev/takasqr/image/upload/l_dylagecz4vyaeo3xqcdx/l_text:MPLUS1p-Black.ttf_80_bold:ESLint Plugin%0APattern Rules,co_rgb:374151,w_620,c_fit/v1712091289/zq6hxcih0jcwdw2pantm.png"
+        alt="pattern-rules"
+        loading="lazy"
+      >
+    </a>
+
+    <a href="https://github.com/takasqr/hono-firebase-functions">
+      <img
+        src="https://image.hirameki.dev/takasqr/image/upload/l_dylagecz4vyaeo3xqcdx/l_text:MPLUS1p-Black.ttf_80_bold:Hono%0AFirebase Functions,co_rgb:374151,w_620,c_fit/v1712091289/Rectangle_2-2_d9ttf8.webp"
+        alt="Hono Firebase Functions"
+        loading="lazy"
+      >
+    </a>
+
+    <a href="https://github.com/takasqr/vanilla-vue-ui">
+      <img
+        src="https://image.hirameki.dev/takasqr/image/upload/l_dylagecz4vyaeo3xqcdx/l_text:MPLUS1p-Black.ttf_80_bold:Vanilla%0AVue%0AUi,co_rgb:374151,w_620,c_fit/v1712091289/zq6hxcih0jcwdw2pantm.png"
+        alt="Vanilla Vue Ui"
+        loading="lazy"
+      >
+    </a>
+  </Carousel>
+</template>
+
+<script setup lang="ts">
+import Carousel from '@takasqr/tw-vue-ui/basic/carousel/Carousel.vue'
+</script>
--- pages/ja/blog/index.vue
+++ pages/ja/blog/index.vue
@@ -42,23 +42,7 @@
           <div class="mb-2">
             <span class="text-gray-600">ライブラリ作りました＼(^o^)／</span>
           </div>
-          <Carousel class="rounded-xl">
-            <a href="https://japanjs.org">
-              <img
-                src="https://image.hirameki.dev/takasqr/image/upload/l_dylagecz4vyaeo3xqcdx/l_text:MPLUS1p-Black.ttf_100_bold:JapanJS%0A日本関連%0A便利ツール,co_rgb:374151,w_620,c_fit/v1712091289/ttpfb67cwdp2pwjes6pw.png"
-                alt="JapanJS"
-                loading="lazy"
-              >
-            </a>
-
-            <a href="https://pattern-rules.hirameki.dev/ja">
-              <img
-                src="https://image.hirameki.dev/takasqr/image/upload/l_dylagecz4vyaeo3xqcdx/l_text:MPLUS1p-Black.ttf_80_bold:ESLint Plugin%0APattern Rules,co_rgb:374151,w_620,c_fit/v1712091289/zq6hxcih0jcwdw2pantm.png"
-                alt="pattern-rules"
-                loading="lazy"
-              >
-            </a>
-          </Carousel>
+          <MyLibraryCarousel />
         </div>
       </Container>
 
@@ -88,12 +72,12 @@
 
 <script setup lang="ts">
 import Container from '@takasqr/tw-vue-ui/basic/container/Container.vue'
-import Carousel from '@takasqr/tw-vue-ui/basic/carousel/Carousel.vue'
 import ArticlesHomepage from '../../../components/content/articles-homepage/ArticlesHomepage.vue'
 import ArticlesRecently from '../../../components/content/articles-recently/ArticlesRecently.vue'
 import AdPomodoroTree from '../../../components/custom/pomodorotree/AdPomodoroTree.vue'
 import { useSetHead } from '#imports'
 import MyProfileCard from '../../../components/custom/my-profile-card/MyProfileCard.vue'
+import MyLibraryCarousel from '../../../components/custom/my-library-carousel/MyLibraryCarousel.vue'
 
 const route = useRoute()
 const lang = 'ja'
