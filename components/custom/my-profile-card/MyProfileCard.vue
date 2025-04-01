13bfb1db023571ea3be7237c892622e70fa797f7
takasqr <95556025+takasqr@users.noreply.github.com>
2025-04-02 06:17:14 +0900

    chore: 自己紹介追加

--- components/custom/my-profile-card/MyProfileCard.vue
+++ components/custom/my-profile-card/MyProfileCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-64">
-    <ProfileCard name="かい (@takasqr)" role="ソフトウェアエンジニア" image-url="/profile.webp" x-url="https://x.com/takasqr" />
+    <ProfileCard name="かい (@takasqr)" role="ソフトウェアエンジニア。個人的にアプリを作って得た知見をブログに書いています。" image-url="/profile.webp" x-url="https://x.com/takasqr" />
   </div>
 </template>
 
