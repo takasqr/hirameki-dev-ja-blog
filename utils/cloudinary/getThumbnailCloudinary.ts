b0619f5f64d3ccc8853c9cda58b859e6d0f2a8d5
takasqr <95556025+takasqr@users.noreply.github.com>
2025-05-16 04:32:38 +0900

    fix: サムネイル画像のサイズを調整

--- utils/cloudinary/getThumbnailCloudinary.ts
+++ utils/cloudinary/getThumbnailCloudinary.ts
@@ -12,7 +12,7 @@ function getThumbnailCloudinary(url: string): string {
   const lastSlashIndex = urlWithoutVersion.lastIndexOf('/');
   if (lastSlashIndex === -1) return urlWithoutVersion; // スラッシュがない場合はそのまま返す
 
-  const newWidth = 200
+  const newWidth = 350
 
   return (
     urlWithoutVersion.slice(0, lastSlashIndex) +
