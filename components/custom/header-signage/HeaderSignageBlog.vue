d9b32d706c978c3abef09105bc11a8b9c47f6ef5
takasqr <95556025+takasqr@users.noreply.github.com>
2025-05-30 21:30:51 +0900

    fix: 内部リンクを解除

--- components/custom/header-signage/HeaderSignageBlog.vue
+++ components/custom/header-signage/HeaderSignageBlog.vue
@@ -1,18 +1,17 @@
 <template>
   <span>
     <a class="underline font-medium" href="/ja/blog/journal/2025/20250309_built-custom-keyboard/">キーボードを自作した話</a>
-    <!-- <a class="underline font-medium" href="/ja/blog/journal/2025/20250210_learn_9_years">9年間毎日プログラミングした話</a> -->
 
     <br class="md:hidden">
 
     <span class="ml-0 md:ml-4 mr-1">by</span>
-    <a href="/ja/">
-      <img
+    <!-- <a href="/ja/"> -->
+    <img
       class="h-4 inline m-0"
       src="/ひらめき開発.webp"
-      alt=""
-      >
-    </a>
+      alt="ひらめき開発"
+    >
+    <!-- </a> -->
 
     <a class="underline font-medium ml-2" href="/ja/blog/journal/">日常</a>
     <a class="underline font-medium ml-2" href="/ja/blog/tech/">開発</a>
