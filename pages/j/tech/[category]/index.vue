612e86f2480d3c01fef5f8414d26e4c6a961f70e
takasqr <95556025+takasqr@users.noreply.github.com>
2025-06-13 20:27:53 +0900

    feat: ディレクトリ変更をテスト

---
+++ pages/j/tech/[category]/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <NuxtLayout>
+    <template #title>
+      <div class="prose lg:prose-xl">
+        <h1>{{ category }}</h1>
+      </div>
+    </template>
+
+    <CategoryPageView :category="categoryString" />
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+import CategoryPageView from '../../../../../components/content/category-page-view/CategoryPageView.vue'
+
+const { category } = useRoute().params
+const categoryString = category as unknown as string
+
+
+definePageMeta({
+  layout: 'ja-blog-default',
+  middleware: ['trailing-slash']
+})
+
+useSetHead(getTechBlogCategoryHeadParams(categoryString))
+</script>
