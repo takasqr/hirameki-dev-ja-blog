82c35194984dc9e7fff1a17b92808bfad0292c28
takasqr <95556025+takasqr@users.noreply.github.com>
2025-09-26 10:23:22 +0900

    Update getThumbnailCloudinary.test.ts

--- utils/cloudinary/getThumbnailCloudinary.test.ts
+++ utils/cloudinary/getThumbnailCloudinary.test.ts
@@ -4,7 +4,7 @@ import { getThumbnailCloudinary } from './getThumbnailCloudinary'
 describe('isCloudinary', () => {
   it('正常系', () => {
     const url = 'https://image.hirameki.dev/takasqr/image/upload/l_text:MPLUS1p-Black.ttf_70_bold:2025年は%0A文章をたくさん%0A書きたい,co_rgb:fff,w_620,c_fit/v1712091289/cover_2025_rpwog8.png'
-    const expectedUrl = 'https://image.hirameki.dev/takasqr/image/upload/l_text:MPLUS1p-Black.ttf_70_bold:2025年は%0A文章をたくさん%0A書きたい,co_rgb:fff,w_620,c_fit/w_350/cover_2025_rpwog8.png'
+    const expectedUrl = 'https://image.hirameki.dev/takasqr/image/upload/l_text:MPLUS1p-Black.ttf_70_bold:2025年は%0A文章をたくさん%0A書きたい,co_rgb:fff,w_620,c_fit/w_250/cover_2025_rpwog8.png'
 
     const result = getThumbnailCloudinary(url)
 
