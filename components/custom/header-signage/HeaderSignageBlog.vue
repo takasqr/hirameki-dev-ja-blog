2da5578dde8d97051e108477aae19a89939cc717
takasqr <95556025+takasqr@users.noreply.github.com>
2025-10-31 04:37:37 +0900

    Update HeaderSignageBlog.vue

--- components/custom/header-signage/HeaderSignageBlog.vue
+++ components/custom/header-signage/HeaderSignageBlog.vue
@@ -5,13 +5,16 @@
     <!-- <br class="md:hidden"> -->
 
     <span class="mr-1">by</span>
-    <!-- <a href="/ja/"> -->
-    <img
-      class="h-4 inline m-0"
-      src="/logo/hirameki.dev.webp"
-      alt="hirameki.dev"
-    >
-    <!-- </a> -->
+
+    <a href="/ja/">
+      <img
+        class="h-4 w-auto m-0 align-middle inline"
+        src="/logo/hirameki.dev.webp"
+        alt="hirameki.dev"
+        width="339"
+        height="44"
+      >
+    </a>
 
     <!-- <a class="underline font-medium ml-2" href="/p/journal/">日常</a> -->
     <!-- <a class="underline font-medium ml-2" href="/p/tech/">開発</a> -->
