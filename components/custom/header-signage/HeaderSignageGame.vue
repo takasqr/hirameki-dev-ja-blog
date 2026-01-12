bba4d363563bf148ca7cbb7b30ef142ddb477f6a
takasqr <95556025+takasqr@users.noreply.github.com>
2026-01-13 05:58:55 +0900

    Update HeaderSignageGame.vue

--- components/custom/header-signage/HeaderSignageGame.vue
+++ components/custom/header-signage/HeaderSignageGame.vue
@@ -1,6 +1,6 @@
 <template>
   <GameDialog
-      text="アプリ + 文章で「体験できるメディア」"
+      text="アプリ + 文章で「体験できるWebメディア」"
       link='/ja/blog/journal/2025/20250513_about-this-blog/'
       link-label='詳しく'
       :speed=130
